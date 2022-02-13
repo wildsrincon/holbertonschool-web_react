5f5ddc5d1868110b0515f65bad623b5466a9bb72
Wilder Rincon <1588@holbertonschool.com>
2022-02-13 11:27:12 -0500

    solution error webpack file

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public")
-  }
+    path: path.resolve(__dirname, 'public')
+  },
 };
